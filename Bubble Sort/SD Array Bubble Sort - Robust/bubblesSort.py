dd1d9379169a760c00ae0761b886200cf0c94070
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2020-05-19 03:16:57 +0500

    bubble sort python code
    Robust bubble sort code

---
+++ Bubble Sort/SD Array Bubble Sort - Robust/bubblesSort.py
@@ -0,0 +1,24 @@
+def bubbleSort(array, length):
+    isSorted = True
+    i = 0
+    j = 0
+    temp = 0
+    for i in range(length-1, 0, -1):
+        isSorted = True
+        for j in range(i):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+                isSorted = False
+        if isSorted == True: break
+
+array = [0 for i in range(10)]
+count = 0
+for count in range(10):
+    array[count] = int(input("Enter vlaue " + str(count) + ": "))
+
+bubbleSort(array, 10)
+
+for x in array:
+    print(x)
